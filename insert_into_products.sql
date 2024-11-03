bd327d9190fd8dd9d74c83084b4f3287924172db
Sivaprakash <prakashboy99@gmail.com>
2024-11-03 19:15:36 +0800

    added database

---
+++ insert_into_products.sql
@@ -0,0 +1,89 @@
+-- Insert TV products
+INSERT INTO
+    products (
+        product_type,
+        product_name,
+        price
+    )
+VALUES ('TV', 'TeX TV XXX', 800),
+    ('TV', 'Prism+ Google TV', 700),
+    ('TV', 'Windows ULTRA', 650),
+    ('TV', 'Samsung OLED TV', 1300),
+    ('TV', 'LG 4K TV', 900),
+    ('TV', 'Tex Crystal Plus', 750);
+
+-- Insert Laptops & PCs products
+INSERT INTO
+    products (
+        product_type,
+        product_name,
+        price
+    )
+VALUES (
+        'Laptops&PCs',
+        'TeX Laptop',
+        1000
+    ),
+    (
+        'Laptops&PCs',
+        'LG Laptop',
+        850
+    ),
+    (
+        'Laptops&PCs',
+        'Razer Gaming Laptop',
+        1500
+    ),
+    (
+        'Laptops&PCs',
+        'Samsung Curved Monitor',
+        400
+    ),
+    (
+        'Laptops&PCs',
+        'Acer Monitor',
+        300
+    ),
+    (
+        'Laptops&PCs',
+        'TeX Curved Ultra',
+        550
+    );
+
+-- Insert Smartphones products
+INSERT INTO
+    products (
+        product_type,
+        product_name,
+        price
+    )
+VALUES (
+        'Smartphones',
+        'iPhone 16',
+        1100
+    ),
+    (
+        'Smartphones',
+        'iPhone 16 Plus',
+        1200
+    ),
+    (
+        'Smartphones',
+        'iPhone 15',
+        1000
+    ),
+    (
+        'Smartphones',
+        'Samsung Galaxy',
+        800
+    ),
+    (
+        'Smartphones',
+        'Google Pixel 4',
+        900
+    ),
+    (
+        'Smartphones',
+        'OPPO Reno 17',
+        650
+    );
---
+++ textable.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS TEX;
+
+CREATE TABLE IF NOT EXISTS usersaccounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_type VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userid INT NOT NULL,
+    product_type VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    product_colour VARCHAR(20) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES usersaccounts (id) ON DELETE CASCADE
+);
